c071df5706a25c71c6bc50c5b747b1f8cc8d211c
Ádám Sipos <adam2458hu@gmail.com>
2020-01-30 19:37:42 +0100

    Initial author routes

---
+++ routes/authors.js
@@ -0,0 +1,45 @@
+const express  = require('express');
+const router = express.Router();
+const Author = require('../models/author');
+
+//All author route
+router.get('/',async(req,res)=>{
+	let searchOptions = {};
+	if (req.query.name!=null && req.query.name!==''){
+		searchOptions.name = new RegExp(req.query.name,'i');
+	}
+
+	try {
+		const authors = await Author.find(searchOptions);
+		res.render('authors/index.ejs',{
+			authors : authors,
+			searchOptions : req.query
+		})
+	} catch(err){
+		res.status(500).json(err);
+	}
+})
+
+//New author form route
+router.get('/new',(req,res)=>{
+	res.render('authors/new.ejs',{author: new Author()});
+})
+
+//create author route
+router.post('/',async(req,res)=>{
+	const author = new Author({
+		name : req.body.name
+	})
+
+	try {
+		await author.save();
+		res.redirect('authors');
+	} catch(err){
+		res.render('authors/new.ejs',{
+			author: author,
+			message: 'Error creating author'
+		});
+	}
+})
+
+module.exports = router;
